ce988e885d879eb6b8c53583c8dee2c13a08f891
churjan <churjan@163.com>
2018-07-06 11:15:43 +0800

    'add'

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
-  // let zhihu=document.getElementById('zhihu');
-  // util.autoPlayAudio(zhihu);
-  //     zhihu.play()
-  //     document.addEventListener('touchstart', function(){
-  //         zhihu.play();
-  //     }, false);
-
-  let myScroll = new IScroll("#iscroll", {
-    scrollY: true,
-    bounce:false,
-    momentum:0,
-    startY:-1000
-  });
-
+ 
  
  
 });
--- src/js/util.js
+++ src/js/util.js
@@ -1,7 +1,35 @@
 const util={
+    //图片加载
+    loading(rootPath,imgArr,callback){
+        let total=imgArr.length;//图片总数量
+        let count=0;//图片每加载一个数量加一
+        imgArr.forEach((item)=>{
+          let img=new Image();
+          img.src=rootPath+item;
+          img.onload=function() {
+            count++;
+            callback(count,total);
+          }
+        })
+    },
+    //在微信里面自动播放
+    weixinAutoPlay(el){
+        el.play();
+        if(window.WeixinJSBridge){
+            WeixinJSBridge.invoke('getNetworkType', {}, function(e) {
+                el.play();
+            }, false);
+        }else{
+            document.addEventListener("WeixinJSBridgeReady", function() {
+                WeixinJSBridge.invoke('getNetworkType', {}, function(e) {
+                    el.play();
+                });
+            }, false);
+        }
+        
+      },
     //获取地址栏的params
-    getUrlQuery(str) {
-        str = str === null ? window.location.href : str;
+    getUrlQuery(str=window.location.href) {
         let arr = str.substring(str.lastIndexOf("?") + 1).split("&");
         let obj = {};
         for (let i = 0; i < arr.length; i++) {
@@ -9,24 +37,5 @@ const util={
             obj[newArr[0]] = newArr[1];
         }
         return obj;
-    },
-    
-    //在微信里面自动播放
-    autoPlayAudio(ele){
-        if(window.WeixinJSBridge){
-            WeixinJSBridge.invoke('getNetworkType', {}, function(e) {
-                ele.play();
-                ele.pause();
-                ele.play();
-            }, false);
-        }else{
-            document.addEventListener("WeixinJSBridgeReady", function() {
-                WeixinJSBridge.invoke('getNetworkType', {}, function(e) {
-                    ele.play();
-                    ele.pause();
-                    ele.play();
-                });
-            }, false);
-        }
-      }
+    }
 }
